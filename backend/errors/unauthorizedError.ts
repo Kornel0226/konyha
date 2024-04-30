4de8a6cffbbb06e4bba9c11c841528f7d2e0c24b
kkory02 <r6kory@gmail.com>
2024-04-30 14:10:01 +0200

    feat: Add UnauthorizedError class and update authentication middleware
    Added a new UnauthorizedError class to handle unauthorized access errors in the backend.
    Updated the authentication middleware to utilize the UnauthorizedError class for better error handling.

---
+++ backend/errors/unauthorizedError.ts
@@ -0,0 +1,11 @@
+import StatusCodes from "http-status-codes"
+import CustomAPIError from "./custom-error";
+
+
+class UnauthorizedError extends CustomAPIError {
+    constructor(message: string) {
+        super(message, StatusCodes.UNAUTHORIZED)
+    }
+}
+
+export default UnauthorizedError;
--- backend/middleware/authentication.ts
+++ backend/middleware/authentication.ts
@@ -1,9 +1,49 @@
 import { RequestHandler } from "express"
+import UnauthorizedError from "../errors/unauthorizedError";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import InternalServerError from '../errors/internal-server-error';
 
-const authentication: RequestHandler = (req, res) => {
-    const { email, password } = req.query
-    console.log(req.query);
-    res.end();
+const authentication: RequestHandler = async (req, res, next) => {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader || !authHeader.startsWith("Bearer ") !){
+        return next(new UnauthorizedError("Not logged in!"))
+    }
+
+    const token = authHeader.split(" ")[1]
+
+    const secret = process.env.JWT_SECRET
+
+    if (!secret){
+        return next(new InternalServerError("Opss, something unexpected happened."))
+    }
+
+    let decodedToken: JwtPayload | string;
+    try {
+        decodedToken = jwt.verify(token, secret)     
+    } catch (error) {
+        return next(new UnauthorizedError("You have bo access!"))
+    }
+
+    if (!isJwtPayload(decodedToken)){
+        return next(new UnauthorizedError("You have no access!"))
+    }
+
+    const {id, username} = decodedToken
+
+    if (!id || !username) {
+        return next(new UnauthorizedError("You have no access!"))
+    }
+
+    req.body.user = {
+        id, username
+    }
+
+    next();
 }
-
 export default authentication;
+
+// Custom type guard function
+function isJwtPayload(token: JwtPayload | string): token is JwtPayload {
+    return typeof token !== 'string';
+}
