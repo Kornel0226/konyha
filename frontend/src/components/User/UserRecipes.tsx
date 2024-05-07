2ca67db3ad6bcc8d5defa27fd2a58861e0286f38
Kornel0226 <r6kory@gmail.com>
2024-05-07 20:25:20 +0200

    Vizsgaremek

--- frontend/src/components/User/UserRecipes.tsx
+++ frontend/src/components/User/UserRecipes.tsx
@@ -8,12 +8,13 @@ const UserRecipes: FC<{ recipes: Recipe[], deleteRecipe: (recipe_id: number) => 
     //200x 304
     const navigate = useNavigate()
 
-    return <div className="bg-orange-300 min-h-[100vh] h-[115vh] p-10">
+    return <div className="bg-orange-300 min-h-[100vh] lg:min-h-[120vh] p-10">
         <h1 className="text-[3rem] lg:text-[5rem] lg:text-left text-orange-500 font-bold mb-10">Recepteim</h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-5 gap-10 lg:gap-2">
+            <button onClick={() => navigate("új")} className="cursor-pointer h-max min-h-[35vh] flex flex-col justify-center items-center w-[100%] lg:max-w-[200px] p-1 rounded-xl m-1 bg-orange-600 hover:transform hover:scale-110 transition-transform duration-300 ease-in-out" ><img className="size-52" src={plus} alt="add" />
+                <p className=" text-2xl pb-2 lg:text-4xl font-semibold">Új recept</p></button>
             {recipes.map(recipe => <RecipeItem isOwn={true} key={recipe.recipe_id} deleteRecipe={() => deleteRecipe(recipe.recipe_id)} recipe={recipe} />)}
-            <button onClick={() => navigate("új")} className=" w-full lg:w-[200px] lg:h-[304px] rounded-xl bg-orange-600 flex flex-col text-center items-center hover:transform hover:scale-110 transition-transform duration-300 ease-in-out" ><img className="size-52" src={plus} alt="add" />
-                <p className="text-center text-2xl pb-2 lg:text-4xl font-semibold">Új recept</p></button>
+
         </div>
     </div>
 }
